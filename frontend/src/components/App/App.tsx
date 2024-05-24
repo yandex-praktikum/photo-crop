cc7cd977a70d4035ecc80868806b54ee4925dcce
Alex Martynov <fenixdeveloper@gmail.com>
2024-05-24 08:38:36 +0200

    feat: realize local photo cropping in App component

--- frontend/src/components/App/App.tsx
+++ frontend/src/components/App/App.tsx
@@ -1,9 +1,161 @@
+import { useRef, useState } from 'react';
+import clsx from 'clsx';
+
+import { FileUpload } from '../FileUpload/FileUpload.tsx';
+import { ImagePreview, ImageSize } from '../ImagePreview/ImagePreview.tsx';
+import { Resizer } from '../Resizer/Resizer.tsx';
+import {
+	Box,
+	getScale,
+	isBoxInside,
+	scaleBox,
+	scaleSize,
+	Size,
+} from '../../utils/box.ts';
+import { crop, download, throttle } from '../../utils/utils.ts';
+import { Actions } from '../Actions/Action.tsx';
+
 import styles from './App.module.scss';
 
 function App() {
+	// Сохраняем файлы, которые загрузил пользователь,
+	// объекты должны быть неизменными иначе потеряем к ним доступ
+	const [files, setFiles] = useState<File[]>([]);
+	// Сохраняем координаты и размеры области для обрезки
+	const [box, setBox] = useState<Box>({
+		x: 0,
+		y: 0,
+		width: 0,
+		height: 0,
+	});
+	// Сохраняем размеры изображения и минимальные и максимальные размеры области для обрезки
+	const [size, setSize] = useState<{
+		image: ImageSize; // размеры изображения (оригинальное и после отрисовки)
+		min: Size;
+		max: Size;
+	} | null>(null);
+	// Пока еще нет сервера, а проверить наши вычисления на практике хочется,
+	// поэтому используем ref для доступа к изображению
+	// и передаем его в функцию crop для локальной обрезки
+	const ref = useRef<HTMLImageElement | null>(null);
+
+	// Действия при нажатии на кнопку "Crop and download"
+	const onCrop = () => {
+		const cropSize = scaleBox(
+			box,
+			getScale(size!.image.rendered, size!.image.natural)
+		);
+		try {
+			download(crop(ref.current!, cropSize), 'cropped.png');
+		} catch (e) {
+			console.error(e);
+		}
+	};
+
+	// Действия при нажатии на кнопку "Reset"
+	const onReset = () => {
+		setFiles([]);
+		setSize(null);
+		setBox({
+			x: 0,
+			y: 0,
+			width: 0,
+			height: 0,
+		});
+	};
+
+	// Пересчитываем масштаб области для обрезки при изменении размеров окна
+	const onScale = throttle((image: ImageSize) => {
+		if (size) {
+			setSize((prev) => {
+				const scale = getScale(size.image.rendered, image.rendered);
+				console.log(scale);
+				setBox((prev) => scaleBox(prev, scale));
+				return {
+					...prev,
+					image,
+					min: scaleSize(prev!.min, scale),
+					max: scaleSize(prev!.max, scale),
+				};
+			});
+		}
+	}, 100);
+
+	// Обработчик изменения размера окна браузера
+	const onResize = (image: ImageSize) => {
+		// Первый раз устанавливаем размеры изображения и области для обрезки
+		if (size === null) {
+			setSize({
+				image,
+				min: {
+					width: 50,
+					height: 50,
+				},
+				max: {
+					width: image.rendered.width,
+					height: image.rendered.height,
+				},
+			});
+			setBox({
+				x: 0,
+				y: 0,
+				width: image.rendered.width,
+				height: image.rendered.height,
+			});
+		} else onScale(image); // Дальше, при изменении размеров окна, пересчитываем масштаб
+	};
+
 	return (
 		<main className={styles.app}>
-			<h1>React App</h1>
+			{files[0] ? (
+				<div className={styles.container}>
+					<ImagePreview
+						ref={ref}
+						file={files[0]}
+						setSize={onResize}
+						className={styles.preview}
+					/>
+					{size && (
+						<Resizer
+							value={box}
+							onChange={setBox}
+							className={styles.resizer}
+							min={size.min}
+							max={size.max}
+						/>
+					)}
+				</div>
+			) : (
+				<FileUpload
+					value={files}
+					onChange={setFiles}
+					fileTypes={['image/jpeg', 'image/png']}
+					maxSize={1024 * 1024 * 2}
+					className={styles.dropzone}
+				/>
+			)}
+
+			<Actions
+				className={styles.actions}
+				items={[
+					{
+						className: clsx('yellow', 'button'),
+						label: 'Crop and download',
+						onClick: onCrop,
+						isDisabled: !(
+							files.length !== 0 &&
+							size?.max &&
+							isBoxInside(box, size.max)
+						),
+					},
+					{
+						className: clsx('fuchsia', 'button'),
+						label: 'Reset',
+						onClick: onReset,
+						isDisabled: files.length === 0,
+					},
+				]}
+			/>
 		</main>
 	);
 }
--- frontend/src/utils/utils.ts
+++ frontend/src/utils/utils.ts
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Box } from './box.ts';
 
 /**
  * Функция для проверки, является ли значение MutableRefObject
@@ -26,4 +27,57 @@ export function throttle<T extends (...args: never[]) => void>(
 		last = now;
 		fn(...args);
 	};
+}
+
+/**
+ * Функция для обрезки изображения прямо в браузере
+ * @param image dom-элемент с загруженным изображением
+ * @param box объект с координатами и размерами области для обрезки
+ */
+export function crop(image: HTMLImageElement, box: Box): string {
+	// Создаем холст и получаем 2d контекст для редактирования
+	const canvas = document.createElement('canvas');
+	const ctx = canvas.getContext('2d');
+
+	// 2D контекст есть почти всегда, но на всякий случай проверим
+	if (ctx) {
+		// Устанавливаем размеры холста
+		canvas.width = box.width;
+		canvas.height = box.height;
+
+		// Рисуем изображение на холсте
+		ctx.drawImage(
+			image, // источник изображения, откуда берем пиксели для рисования
+			box.x, // координата x (image), с которой начинаем рисовать
+			box.y, // координата y (image), с которой начинаем рисовать
+			box.width, // ширина (image), которую рисуем
+			box.height, // высота (image), которую рисуем
+			0, // координата x (canvas), куда начинаем рисовать
+			0, // координата y (canvas), куда начинаем рисовать
+			box.width, // ширина (canvas), которую рисуем
+			box.height // высота (canvas), которую рисуем
+		);
+
+		// Возвращаем изображение в формате base64
+		return canvas.toDataURL('image/png');
+	} else {
+		throw new Error('Could not get 2d context');
+	}
+}
+
+/**
+ * Функция для форсированного скачивания файла по URL
+ * через эмуляцию клика по ссылке
+ * @param url адрес или base64-строка файла
+ * @param name имя файла
+ */
+export function download(url: string, name: string) {
+	const a = document.createElement('a');
+	a.href = url;
+	// Устанавливаем атрибут download, чтобы браузер понял, что нужно скачать файл
+	// Если установить свое имя сгенерированное в коде, то файл будет скачиваться с этим именем
+	// Но если используем что-то с сервера, например имя из заголовков,
+	// то например Chrome добавит к имени _ (например _file.jpg_) в начале и в конце
+	a.download = name;
+	a.click();
 }
