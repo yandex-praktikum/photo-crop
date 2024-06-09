43dc09af0dc8147612ff8ec435476331530594bc
Alex Martynov <fenixdeveloper@gmail.com>
2024-06-09 14:43:50 +0200

    feat: rebuild app for working with multiple files

--- frontend/src/components/App/App.tsx
+++ frontend/src/components/App/App.tsx
@@ -1,9 +1,8 @@
-import { useReducer, useState } from 'react';
+import { useEffect, useReducer } from 'react';
 import clsx from 'clsx';
 
 import { FileUpload } from '../FileUpload/FileUpload.tsx';
 import { getScale, isBoxInside, scaleBox } from '../../utils/box.ts';
-import { remoteCrop } from '../../utils/utils.ts';
 import { Actions } from '../Actions/Action.tsx';
 
 import styles from './App.module.scss';
@@ -11,32 +10,71 @@ import { Cropper } from '../Cropper/Cropper.tsx';
 import { cropReducer, InitialCropState } from '../Cropper/Reducer.tsx';
 import { Selector } from '../Selector/Selector.tsx';
 import { ImagePreview } from '../ImagePreview/ImagePreview.tsx';
+import { useFileUploader } from '../../hooks/useFileUploader.tsx';
+import { CropResult, pull, sendCropTask } from '../../utils/api.ts';
 
 function App() {
 	// Сохраняем файлы, которые загрузил пользователь,
 	// объекты должны быть неизменными иначе потеряем к ним доступ
-	const [files, setFiles] = useState<File[]>([]);
+	const { files, setFiles, getFileID } = useFileUploader({
+		timeout: 60000,
+	});
 	const [state, dispatch] = useReducer(cropReducer, InitialCropState);
 
-	// Действия при нажатии на кнопку "Crop and download"
-	const onCrop = () => {
-		const cropSize = scaleBox(
-			state.cropSettings[state.selected].box,
-			getScale(
-				state.cropSettings[state.selected].size!.image.rendered,
-				state.cropSettings[state.selected].size!.image.natural
-			)
-		);
-		// Если в настройках установлен сервер, отправляем запрос на обрезку
-		if (import.meta.env.VITE_CROP_API) {
-			// @todo: можно было бы проверять доступность сервера
-			void remoteCrop(import.meta.env.VITE_CROP_API, files[state.selected], cropSize);
+	useEffect(() => {
+		if (files.length === 0) {
+			dispatch({ type: 'RESET' });
 		}
+		if (files.length > state.cropSettings.length) {
+			dispatch({
+				type: 'ADD_CROPPER',
+				payload: files.length - state.cropSettings.length,
+			});
+		}
+	}, [files, state.cropSettings.length]);
+
+	const downloadArchive = async () => {
+		const task = state.cropSettings.map((crop, index) => {
+			return {
+				photo: getFileID(files[index])!,
+				box: scaleBox(
+					crop.box,
+					getScale(crop.size!.image.rendered, crop.size!.image.natural)
+				),
+				fileName: files[index].name,
+			};
+		});
+
+		const id = await sendCropTask(import.meta.env.VITE_CROP_API, task);
+
+		try {
+			await pull<CropResult>([import.meta.env.VITE_CROP_API, id].join('/'), {
+				timeout: 15000,
+			});
+			return [import.meta.env.VITE_CROP_API, id, 'result'].join('/');
+		} catch (err) {
+			console.log(err);
+		}
+	};
+
+	// Действия при нажатии на кнопку "Crop and download"
+	const onCrop = async () => {
+		const archiveUrl = await downloadArchive();
+		window.open(archiveUrl);
 	};
 
 	// Действия при нажатии на кнопку "Reset"
 	const onReset = () => {
 		dispatch({ type: 'RESET' });
+	};
+
+	const isAllCropped = () => {
+		let isReady = true;
+		state.cropSettings.forEach((crop) => {
+			isReady =
+				isReady && !!crop.size?.max && isBoxInside(crop.box, crop.size!.max);
+		});
+		return isReady;
 	};
 
 	return (
@@ -50,7 +88,7 @@ function App() {
 					value={files}
 					onChange={setFiles}
 					fileTypes={['image/jpeg', 'image/png']}
-					maxSize={1024 * 1024 * 2}
+					maxSize={1024 * 1024 * 10}
 					maxCount={10}
 					className={styles.dropzone}
 				/>
@@ -65,12 +103,16 @@ function App() {
 				{({ index, className }) => (
 					<ImagePreview
 						file={files[index]}
-						className={className}
-						placeholder={<img
-							className={className}
-							src={'https://placehold.co/300x300?text=?'}
-							alt={`Preview ${index}`}
-						/>}
+						className={clsx(className, styles.icon, {
+							[styles.uploaded]: !!getFileID(files[index]),
+						})}
+						placeholder={
+							<img
+								className={className}
+								src={'https://placehold.co/300x300?text=?'}
+								alt={`Preview ${index}`}
+							/>
+						}
 					/>
 				)}
 			</Selector>
@@ -80,16 +122,9 @@ function App() {
 				items={[
 					{
 						className: clsx('yellow', 'button'),
-						label: 'Crop and download',
+						label: 'Crop all and download .ZIP archive',
 						onClick: onCrop,
-						isDisabled: !(
-							files.length !== 0 &&
-							state.cropSettings[state.selected].size?.max &&
-							isBoxInside(
-								state.cropSettings[state.selected].box,
-								state.cropSettings[state.selected].size!.max
-							)
-						),
+						isDisabled: !(files.length !== 0 && isAllCropped()),
 					},
 					{
 						className: clsx('fuchsia', 'button'),
